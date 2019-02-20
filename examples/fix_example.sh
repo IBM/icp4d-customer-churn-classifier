e9cec72b9ae12cc7e8afd4c673e36950afed38f6
Mark Sturdevant <mark.sturdevant@ibm.com>
2019-02-20 15:14:08 -0800

    Add video, clean-up references to ML, tweak script.
    * Added video
    * Clean up references to WML. Not mentioning Watson here.
    * The curl example was mixed up with Watson. Try to simplify
      the API test by sticking w/ the UI for test and generated code.
    * For the fix_example.sh script "set -i" was supposed to be "set -x"
      for tracing, but decided to remove tracing anyway.

--- examples/fix_example.sh
+++ examples/fix_example.sh
@@ -2,8 +2,6 @@
 
 echo "Tweaking the CSS in the notebook to work with public github viewer."
 
-set -i
 cat TradingCustomerChurnClassifierSparkML.jupyter-py36.ipynb | sed -e 's#/dsx-jupyter-py36/.*/.*/nbextensions/brunel_ext/#https://brunelvis.org/js/#g' > fixed.ipynb
-set +i
 
 echo "Saved as 'fixed.ipynb' for you to diff, copy, push..."
