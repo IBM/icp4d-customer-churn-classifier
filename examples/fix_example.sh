87e9ff0691aa403d67dde60bcbc20b51d642cfec
Sandhya Nayak <sandhya.n.nayak@gmail.com>
2020-12-09 13:45:52 -0500

    Update script and notebook to show visualizations

--- examples/fix_example.sh
+++ examples/fix_example.sh
@@ -2,6 +2,6 @@
 
 echo "Tweaking the CSS in the notebook to work with public github viewer."
 
-cat TradingCustomerChurnClassifierSparkML.jupyter-py36.ipynb | sed -e 's#/dsx-jupyter-py36/.*/.*/nbextensions/brunel_ext/#https://brunelvis.org/js/#g' > fixed.ipynb
+cat TradingCustomerChurnClassifierSparkML.ipynb | sed -e 's#/data/jupyter2/.*/nbextensions/brunel_ext/brunel.2.3.css#https://brunelvis.org/js/brunel.2.3.css#g' | sed -e 's#/data/jupyter2/.*/nbextensions/brunel_ext/sumoselect.css#https://brunelvis.org/js/sumoselect.css#g'| sed -e 's#/data/jupyter2/.*/nbextensions/brunel_ext/brunel.2.3.min#//brunelvis.org/js/brunel.2.3.min#g' | sed -e 's#/data/jupyter2/.*/nbextensions/brunel_ext/brunel.controls.2.3.min#//brunelvis.org/js/brunel.controls.2.3.min#g' > fixed.ipynb
 
 echo "Saved as 'fixed.ipynb' for you to diff, copy, push..."
